870c37ca533bb811ddffdf270097027b63a0c387
Rabea512 <rdg3@gmx.de>
2024-12-20 21:07:39 +0100

    day 11

--- src/year2024/Day10/Main.java
+++ src/year2024/Day10/Main.java
@@ -3,36 +3,48 @@ package year2024.Day10;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        System.out.println(part1("/Users/degroot/Privat/AdventOfCode/src/year2024/Day10/input_test.txt"));
-        System.out.println(part1("/Users/degroot/Privat/AdventOfCode/src/year2024/Day10/input01.txt"));
+  public static void main(String[] args) throws IOException {
+    System.out.println(
+        part1("/Users/degroot/Privat/AdventOfCode/src/year2024/Day10/input_test.txt"));
+    System.out.println(part1("/Users/degroot/Privat/AdventOfCode/src/year2024/Day10/input01.txt"));
 
-        System.out.println(part2("/Users/degroot/Privat/AdventOfCode/src/year2024/Day10/input_test.txt"));
-        System.out.println(part2("/Users/degroot/Privat/AdventOfCode/src/year2024/Day10/input02.txt"));
-    }
+    System.out.println(
+        part2("/Users/degroot/Privat/AdventOfCode/src/year2024/Day10/input_test.txt"));
+    System.out.println(part2("/Users/degroot/Privat/AdventOfCode/src/year2024/Day10/input02.txt"));
+  }
 
-    public static int part1(String path) throws IOException {
-        return 0;
-    }
+  public static int part1(String path) throws IOException {
+    final Input input = readInput(path);
 
-    public static int part2(String path) throws IOException {
-        return 0;
-    }
 
-    public static Input readInput(String path) throws IOException {
 
-        try (BufferedReader reader = new BufferedReader(
-                new FileReader(path))) {
-            String line = reader.readLine();
-            while (line != null && !line.isEmpty()) {
-                line = reader.readLine();
-            }
+    return 0;
+  }
+
+  public static int part2(String path) throws IOException {
+    return 0;
+  }
+
+  public static Input readInput(String path) throws IOException {
+    final List<List<Integer>> map = new ArrayList<>();
+
+    try (BufferedReader reader = new BufferedReader(new FileReader(path))) {
+      String line = reader.readLine();
+      while (line != null && !line.isEmpty()) {
+        final List<Integer> row = new ArrayList<>();
+        for (int i = 0; i < line.length(); i++) {
+          row.add(Integer.parseInt(line.substring(i, i + 1)));
         }
-        return new Input();
+        map.add(row);
+        line = reader.readLine();
+      }
     }
+    return new Input(map);
+  }
 
-    public record Input(/* Add datastructure here */) {
-    }
+  public record Input(List<List<Integer>> map) {}
 }
--- src/year2024/Day11/Main.java
+++ src/year2024/Day11/Main.java
@@ -3,6 +3,7 @@ package year2024.Day11;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.*;
 
 public class Main {
   public static void main(String[] args) throws IOException {
@@ -12,27 +13,80 @@ public class Main {
 
     System.out.println(
         part2("/Users/degroot/Privat/AdventOfCode/src/year2024/Day11/input_test.txt"));
+
     System.out.println(part2("/Users/degroot/Privat/AdventOfCode/src/year2024/Day11/input02.txt"));
   }
 
-  public static int part1(String path) throws IOException {
-    return 0;
+  public static int part1(final String path) throws IOException {
+    List<Stone> stones = readInput(path).stones();
+
+    for (int i = 0; i < 25; i++) {
+      stones = blink(stones);
+    }
+
+    return stones.size();
   }
 
-  public static int part2(String path) throws IOException {
-    return 0;
+  public static Long part2(final String path) throws IOException {
+    final List<Stone> stones = readInput(path).stones();
+    Map<Stone, Long> stoneMap = new HashMap<>();
+    for (Stone stone1 : stones) {
+      stoneMap.put(stone1, 1L);
+    }
+
+    for (int i = 0; i < 75; i++) {
+      final Map<Stone, Long> newStoneMap = new HashMap<>();
+      stoneMap.forEach(
+          (stone, count) -> {
+            List<Stone> blinkedStones = blinkStone(stone);
+            blinkedStones.forEach(
+                blinkedStone -> {
+                  newStoneMap.putIfAbsent(blinkedStone, 0L);
+                  newStoneMap.put(blinkedStone, newStoneMap.get(blinkedStone) + count);
+                });
+          });
+      stoneMap = newStoneMap;
+    }
+
+    return stoneMap.values().stream().reduce(0L, Long::sum);
   }
 
-  public static Input readInput(String path) throws IOException {
+  private static List<Stone> blink(final List<Stone> stones) {
+    final List<Stone> newStones = new ArrayList<>();
+
+    for (final Stone currentStone : stones) {
+      newStones.addAll(blinkStone(currentStone));
+    }
+
+    return newStones;
+  }
+
+  private static List<Stone> blinkStone(final Stone stone) {
+    if (stone.engraving() == 0) {
+      return List.of(new Stone(1L));
+    } else if (String.valueOf(stone.engraving()).length() % 2 == 0) {
+      final String engraving = String.valueOf(stone.engraving());
+      return List.of(
+          new Stone(Long.parseLong(engraving.substring(0, engraving.length() / 2))),
+          new Stone(Long.parseLong(engraving.substring(engraving.length() / 2))));
+    } else {
+      return List.of(new Stone(stone.engraving() * 2024));
+    }
+  }
+
+  public static Input readInput(final String path) throws IOException {
+    final List<Stone> stones;
 
     try (BufferedReader reader = new BufferedReader(new FileReader(path))) {
-      String line = reader.readLine();
-      while (line != null && !line.isEmpty()) {
-        line = reader.readLine();
-      }
+      final String line = reader.readLine();
+      stones =
+          new ArrayList<>(
+              Arrays.stream(line.split(" ")).map(s -> new Stone(Long.parseLong(s))).toList());
     }
-    return new Input();
+    return new Input(stones);
   }
 
-  public record Input(/* Add datastructure here */ ) {}
+  public record Input(List<Stone> stones) {}
+
+  public record Stone(Long engraving) {}
 }
